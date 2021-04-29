2c6cf107f85dba19cfd46cfb7e841e76899d51ea
Fadil Al <fadil.alfarhan@gmail.com>
2021-04-29 16:23:14 +0200

    initialize git repo and format ComponentA and ComponentB to functional components, centering the text vertically and horizontaly on each component

---
+++ src/components/componentA/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const  ComponentA = () => {
+
+        return (
+            <div className={"component"}>
+                <span className={"component_title"}> Component A </span>
+            </div>
+        )
+    
+}
+
+export default ComponentA
---
+++ src/components/componentB/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ComponentB = () => {
+
+        return (
+            <div className={"component"}>
+                <span className={"component_title"}> Component B </span>
+            </div>
+        )
+    
+}
+
+
+export default ComponentB 
